83282f15b944bf7fb111da82f9c094767e18c246
Ilja MAC <ilja.polonskij@yahoo.de>
2016-04-22 01:45:29 +0200

    Finished the OLS Regression, regrouped and implemented the error analysis including plots

--- studienarbeit/givenExampleFromUQLab.m
+++ studienarbeit/givenExampleFromUQLab.m
@@ -38,45 +38,72 @@ MetaOpts.Input = myInput;
 % Ishigami model that was created
 MetaOpts.FullModel = myModel;
 
-X = uq_getSample(10000,'MC',myInput);
+X = uq_getSample(6300,'MC',myInput);
 
-%% Calculating the coefficients with different methods
-%% Calculation for the gPC using Quadrature
+%% Creation and Calculation of a gPC Quadrature Model
 
-% Used method: Quadrature (old way)to plot the histogram
 MetaOpts.Method = 'Quadrature';
 
-MetaOpts.Degree = 6;
-PCE_Quadrature = uq_createModel(MetaOpts);
-
-% new way
-numbSamples = zeros(1,10);
+numbSamplesQuad = zeros(1,10);
 mean_quad = zeros(1,10);
 sd_quad = zeros(1,10);
 degree = zeros(1,10);
+error_quad = zeros(1,10);
 for k = 1:10
-    MetaOpts.Type = 'uq_metamodel';
-    MetaOpts.MetaType = 'PCE';
-    MetaOpts.PolyTypes = {'Legendre','Legendre','Legendre'};
-    MetaOpts.Method = 'Quadrature';
     MetaOpts.Degree = k;
-    MetaOpts.Input = myInput;
-    MetaOpts.FullModel = myModel;
-    PCE_Quadrature_New = uq_createModel(MetaOpts);
-    numbSamples(k) = PCE_Quadrature_New.ExpDesign.NSamples;
-    mean_quad(k) = PCE_Quadrature_New.PCE.Moments.Mean;
-    sd_quad(k) = sqrt(PCE_Quadrature_New.PCE.Moments.Var);
+    PCE_Quadrature = uq_createModel(MetaOpts);
+    numbSamplesQuad(k) = PCE_Quadrature.ExpDesign.NSamples;
+    mean_quad(k) = PCE_Quadrature.PCE.Moments.Mean;
+    sd_quad(k) = sqrt(PCE_Quadrature.PCE.Moments.Var);
+    error_quad(k) = PCE_Quadrature.Error.normEmpError;
     degree(k) = k;
 end
-% MetaOpts.Degree = 6;
-% PCE_Quadrature = uq_createModel(MetaOpts);
-%% Creation of the Least-Square Model
 
-% Used method: Least-Squares
+%% Creation and Calculation of a gPC Least-Square Model
+
+%Calculation of a OLS regression model by sweeping the polynomial degree
 MetaOpts.Method = 'OLS';
-MetaOpts.ExpDesign.NSamples = 10000;
+MetaOpts.ExpDesign.NSamples = 6300;
 MetaOpts.ExpDesign.Sampling = 'MC';
 
+mean_ols = zeros(1,10);
+sd_ols = zeros(1,10);
+degreeOLS = zeros(1,10);
+sumbSamplesOls = zeros(1,10);
+error_ols_D = zeros(1,10);
+
+for n=1:10
+    MetaOpts.Degree = n;
+    PCE_OLS_D = uq_createModel(MetaOpts);
+    mean_ols(n) = PCE_OLS_D.PCE.Moments.Mean;
+    sd_ols(n) = sqrt(PCE_OLS_D.PCE.Moments.Var);
+    error_ols_D(n) = PCE_OLS_D.Error.LOO;
+    degreeOLS(n) = n;
+end
+
+%Calculation of a Model OLS Regression Model by sweeping the amount of
+%samples ... did not lead to reasonable results...
+% MetaOpts.Method = 'OLS';
+% MetaOpts.Degree = 3;
+% MetaOpts.ExpDesign.Sampling = 'MC';
+%
+% mean_ols_S = zeros(1,30);
+% sd_ols_S = zeros(1,30);
+% numbSamplesOls = zeros(1,30);
+% error_ols_S = zeros(1,30);
+% for h=1000:1000:30000
+%     MetaOpts.ExpDesign.NSamples = h;
+%     PCE_OLS_S = uq_createModel(MetaOpts);
+%     mean_ols_S(h/1000) = PCE_OLS_S.PCE.Moments.Mean;
+%     sd_ols_S(h/1000) = sqrt(PCE_OLS_S.PCE.Moments.Var);
+%     error_ols_S(h/1000) = PCE_OLS_S.Error.LOO;
+%     numbSamplesOls(h/1000) = h;
+% end
+%
+MetaOpts.Method = 'OLS';
+MetaOpts.ExpDesign.NSamples = 6300;
+MetaOpts.ExpDesign.Sampling = 'MC';
+MetaOpts.Degree = 6;
 PCE_OLS = uq_createModel(MetaOpts);
 
 %% Evaluation of the Respective Method
@@ -94,29 +121,29 @@ ver = version;
 if ver(1) == '8'
     
     figure;
-    hist(Y_full);
+    subplot(1,3,1); hist(Y_full);
+    title('Full');
     drawnow
     
-    figure;
-    hist(Y_Quadrature);
+    subplot(1,3,2);hist(Y_Quadrature);
+    title('Quadrature');
     drawnow
     
-    figure;
-    hist(Y_OLS);
+    subplot(1,3,3); hist(Y_OLS);
+    title('OLS');
     drawnow
     
     % Matlab release 2016
 elseif ver(1) == '9'
-    figure;
-    histogram(Y_full,'FaceColor','b');
-    drawnow
     
     figure;
-    histogram(Y_Quadrature,'FaceColor','r');
+    subplot(1,3,1); histogram(Y_full,'FaceColor','b');
     drawnow
     
-    figure;
-    histogram(Y_OLS,'FaceColor','g');
+    subplot(1,3,2); histogram(Y_Quadrature,'FaceColor','r');
+    drawnow
+    
+    subplot(1,3,3); histogram(Y_OLS,'FaceColor','g');
     drawnow
     
 end
@@ -124,35 +151,23 @@ end
 y_mean = 1:size(X,1);
 y_sd = 1:size(X,1);
 y_mean_tot = mean(Y_full,1);
-y_sd_tot = sqrt(1/(length(Y_full)-1) *(sum(Y_full(:)-y_mean_tot)).^2);
-
+%y_sd_tot = sqrt(1/(length(Y_full)-1) *(sum(Y_full(:)-y_mean_tot).^2));
+y_sd_tot = std(Y_full,1);
 
 y_mean_ols = 1:size(X,1);
 y_sd_ols = 1:size(X,1);
-y_mean_tot_ols = mean(Y_OLS);
+y_mean_tot_ols = mean(Y_OLS,1);
 
 for j=1:size(X,1)
     y_mean(j) = mean(Y_full(1:j,1));
-    y_sd(j) = sqrt(1/(length(y_mean(j)-1))*sum(y_mean(j(:))-y_mean_tot).^2);
-        
-    y_mean_ols(j) = mean(Y_OLS(1:j,1));
-    y_sd_ols(j) = sqrt(1/(length(y_mean_ols(j)-1))*sum(y_mean_ols(j(:))-...
-        y_mean_tot_ols).^2);
+    %     y_sd(j) = sqrt(1/(length(y_mean(j)-1))*...
+    %         sum(y_mean(j(:))-y_mean_tot).^2);
+    y_sd(j) = std(Y_full(1:j,1));
     
-    %     if(j>3)
-    %          eoc_mean_mc(j) = ...
-    %          log10(...
-    %         abs((y_mean(j)-y_mean(j-1))/(y_mean(j-1)-y_mean(j-2))))...
-    %              /...
-    %              log10(...
-    %              abs((y_mean(j-1)-y_mean(j-2))/(y_mean(j-2)-y_mean(j-3))));
-    %        eoc_mean_quad = log10(y_mean_quad(j)/y_mean_quad(j-1))/log10(j/(j-1));
-    %        eoc_mean_ols = log10(y_mean_ols(j)/y_mean_ols(j-1))/log10(j/(j-1));
-    %
-    %        eoc_sd_mc = log10(y_sd(j)/y_sd(j-1))/log10(j/(j-1));
-    %        eoc_sd_quad = log10(y_sd_quad(j)/y_sd(j-1))/log10(j/(j-1));
-    %        eoc_sd_ols = log10(y_sd_ols(j)/y_sd(j-1))/log10(j/(j-1));
-    %       end
+    y_mean_ols(j) = mean(Y_OLS(1:j,1));
+    %     y_sd_ols(j) = sqrt(1/(length(y_mean_ols(j)-1))*sum(y_mean_ols(j(:))-...
+    %         y_mean_tot_ols).^2);
+    y_sd_ols(j) = std(Y_OLS(1:j,1));
 end
 
 %% Plot of the respective mean
@@ -160,9 +175,9 @@ range = 1:size(X,1);
 
 %Comparison Plot with all 3 methods
 figure;
-plot(range,y_mean,'b',numbSamples,mean_quad,'r',...
-    range,y_mean_ols,'g'),
-ylim([2 6]);
+%subplot(4,1,1);
+plot(range,y_mean,'b',numbSamplesQuad,mean_quad,'r',range,y_mean_ols,'g'),   
+%ylim([2 6]);
 xlabel('Amount of Samples'), ylabel('Mean of the Ishigami Output');
 legend('Mean of Y calc. with MC',...
     'Mean of Y calc. with Quadrature PC',...
@@ -170,10 +185,11 @@ legend('Mean of Y calc. with MC',...
 title('Means of the Ishigami Function calc. with Different Methods');
 drawnow
 
-% Plot for MC 
+% Plot for MC
 figure;
+%subplot(4,1,2);
 plot(range,ones(size((range),2))*y_mean_tot);
-ylim([3.25 3.75]);
+%ylim([3.25 3.75]);
 hold on;
 plot(y_mean,'b');
 xlabel('Amount of Samples'), ylabel('Y_{Mean}_{MC}');
@@ -183,15 +199,18 @@ drawnow
 
 % Plot for Quadrature Method
 figure;
-plot(numbSamples,mean_quad,'r');
+%subplot(4,1,3);
+plot(numbSamplesQuad,mean_quad,'r');
 xlabel('Amount of Samples'), ylabel('Mean of Y_Quadrature');
 title('Convergence of the Mean via Quadrature gPC');
 drawnow
 
 % Plot for OLS Regression
 figure;
+%subplot(4,1,4);
 plot(range,ones(size((range),2))*y_mean_tot_ols);
-ylim([2 4]);
+%plot(range,mean_ols);
+%ylim([2 4]);
 hold on;
 plot(y_mean_ols,'g');
 xlabel('Amount of Samples'),ylabel('Y_{Mean}_{OLS}');
@@ -202,8 +221,10 @@ drawnow
 
 %Comparison Plot with all 3 methods
 figure;
-plot(range,y_sd,'b',numbSamples,sd_quad,'r',range,y_sd_ols,'g'),
-ylim([-0.1 0.5])
+%subplot(4,1,1);
+plot(range,y_sd,'b',numbSamplesQuad,sd_quad,'r',range,y_sd_ols,'g');
+    
+%ylim([-0.1 0.5])
 xlabel('Amount of Samples'), ylabel('SD of the Ishigami Output');
 legend('SD of Y calc. with MC','SD of Y calc. with Quadrature PC',...
     'SD of Y calc. with OLS Regression');
@@ -212,8 +233,9 @@ drawnow
 
 % Plot for MC
 figure;
+%subplot(4,1,2);
 plot(range,ones(size((range),2))*y_sd_tot);
-ylim([-0.1 0.25]);
+%ylim([-0.1 0.25]);
 hold on;
 plot(y_sd,'b');
 xlabel('Amount of Samples'), ylabel('Y_{SD}_{MC}');
@@ -223,15 +245,18 @@ drawnow
 
 % Plot for Quadrature Method
 figure;
-plot(numbSamples,sd_quad,'r');
+%subplot(4,1,3);
+plot(numbSamplesQuad,sd_quad,'r');
 xlabel('Amount of Samples'),ylabel('SD of the Y_Quadrature');
 title('Convergence of the SD via Quadrature gPC');
 drawnow
 
 % Plot for OLS Regression
-figure,
+figure;
+%subplot(4,1,4);
 plot(range,ones(size((range),2))*y_sd_tot);
-ylim([-0.1 0.25]);
+%plot(range,sd_mean);
+%ylim([-0.1 0.25]);
 hold on;
 plot(range,y_sd_ols,'g');
 xlabel('Amount of Samples'), ylabel('Y_{SD}_{OLS}');
@@ -239,19 +264,63 @@ title('Convergence of the SD via OLS Regression gPC');
 drawnow
 
 
-%% New Plots for the Quadrature Method
+%% Plots for the Quadrature Method
 
 figure;
-plot(degree,mean_quad);
+subplot(1,3,1);
+plot(degree,mean_quad,'r');
 xlabel('Polynomial Degree'), ylabel('Mean of Y_Quadrature');
 title('Convergence of the Mean via Quadrature gPC');
 drawnow
 
-
-figure;
-plot(degree,sd_quad);
+subplot(1,3,2);
+plot(degree,sd_quad,'r');
 xlabel('Polynomial Degree'),ylabel('SD of Y_Quadrature');
 title('Convergence of the SD via Quadrature gPC');
 drawnow
 
+subplot(1,3,3);
+semilogy(degree,error_quad,'r');
+xlabel('Degree'),ylabel('log(error)');
+title('Quadrature Error');
+
+%% Plots for the OLS Regression Method
+
+figure;
+subplot(1,3,1);
+plot(degreeOLS,mean_ols,'g');
+xlabel('Degrees'),ylabel('Mean of OLS');
+drawnow
+
+subplot(1,3,2);
+plot(degreeOLS,sd_ols,'g');
+xlabel('Degrees'),ylabel('SD of OLS');
+drawnow
+
+subplot(1,3,3);
+semilogy(degreeOLS,error_ols_D,'g');
+xlabel('Degree'),ylabel('log(error)');
+title('OLS Regression Error Depending on the Polynomial Degree');
+
+figure;
+semilogy(degree,error_quad,'r',degreeOLS,error_ols_D,'g');
+xlabel('Polynomial Degree'),ylabel('Log(Error)');
+title(...
+    'Comparision of the Error between the Gaussian Quadrature and the OLS Regression ');
+legend('Gaussian Quadrature','OLS Regression');
+% figure;
+% subplot(1,2,1);
+% plot(numbSamplesOls,mean_ols_S);
+% xlabel('Sample Number'),ylabel('Mean of OLS');
+% drawnow
+%
+% subplot(1,2,2);
+% plot(numbSamplesOls,sd_ols_S);
+% xlabel('Sample Number'),ylabel('SD of OLS');
+% drawnow
+
+% figure;
+% semilogy(numbSamplesOls,error_ols_S);
+% xlabel('Amount of Samples'),ylabel('log(error)');
+% title('OLS Regression Error Depending on the Polynomial Degree');
 
