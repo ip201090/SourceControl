edc1496b0b23334ac14e04becc69846296c00526
=ipTEMF <ilja.polonskij@stud.tu-darmstadt.de>
2016-06-03 18:30:46 +0200

    Added Workflow for the Stern-Gerlach Application

---
+++ Stern-Gerlach-gPC/UQ.m
@@ -0,0 +1,26 @@
+ %% Workflow Script for the Analysis of the Stern-Gerlach Magnet Using gPC
+ 
+%     - Use solve.m to obtain geometry parameter (and others) that are
+%     needed for the next function
+%     - readBFieldValuesGeoPDEs / readBFieldGradientXGeoPDEs
+%     - calculateGradient/calculateGradientHOrder
+%     - calculateAverageMagneticFieldGradient
+%     - calculateFieldHomogeneity/calculateFieldHomogeneityExact
+%     - Setup function (maybe you will have to code the function yourself
+%     - Setup gPC with UQLab
+
+
+%% Determination of the Geometry Parameters 
+
+
+%% Determination of the B-Filed
+
+%% Determination of the B-Field's Gradient
+
+%% Calculation of he Average B-Field Gradient
+
+%% Calculation of the Homogeneity
+
+%% Setup of the Goal Function
+
+%% Setup of the UQ with gPC
